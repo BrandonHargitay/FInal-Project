9dc204ff9b3f94cf28a23820172d6fb42b2addeb
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2022-12-14 18:40:17 -0800

    Add driver for Poker_Analysis_Runner

---
+++ Poker-Analysis/Poker_Analysis_Runner.h
@@ -0,0 +1,21 @@
+//
+// Created by Brandon Hargitay on 12/14/22.
+//
+
+#ifndef FINAL_PROJECT_POKER_ANALYSIS_RUNNER_H
+#define FINAL_PROJECT_POKER_ANALYSIS_RUNNER_H
+
+#include <iostream>
+#include "Drawables/DrawStatistics.h"
+#include "CardHand.h"
+#include "PokerScore.h"
+#include "CardHandScorer.h"
+#include "Drawables/CardDrawable.h"
+#include "Drawables/CardHandDrawable.h"
+class Poker_Analysis_Runner {
+public:
+    void run();
+};
+
+
+#endif //FINAL_PROJECT_POKER_ANALYSIS_RUNNER_H
