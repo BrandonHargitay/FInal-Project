36c488391bbd4b33f39a3055cd02803ff6cde800
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2022-12-14 17:40:38 -0800

    Delete unwanted comments

--- main.cpp
+++ main.cpp
@@ -46,18 +46,3 @@ int main()
 }
 
 
-//cmake_minimum_required(VERSION 3.23)
-//project(Final_Project)
-//
-//set(CMAKE_CXX_STANDARD 14)
-//
-//add_executable(Final_Project Asteroids_Game_Runner-Game/Asteroids_Game_Runner-Game-Runner.cpp CreateWindow.cpp CreateWindow.h Asteroids_Game_Runner-Game/Asteroids_Game_Runner-Game-Runner.h main.cpp Asteroids_Game_Runner-Game/Texture.h Asteroids_Game_Runner-Game/Texture.cpp
-//        Asteroids_Game_Runner-Game/Enums/textureEnum.h Asteroids_Game_Runner-Game/Rocket.cpp Asteroids_Game_Runner-Game/Rocket.h Asteroids_Game_Runner-Game/Asteroids_Game_Runner.cpp Asteroids_Game_Runner-Game/Asteroids_Game_Runner.h Asteroids_Game_Runner-Game/Bullet.cpp
-//        Asteroids_Game_Runner-Game/Bullet.h Asteroids_Game_Runner-Game/ScoreBoard2.cpp Asteroids_Game_Runner-Game/ScoreBoard2.h Asteroids_Game_Runner-Game/Fonts.h Asteroids_Game_Runner-Game/Fonts.cpp Asteroids_Game_Runner-Game/Enums/fontsEnum.h
-//        Asteroids_Game_Runner-Game/Heart.cpp Asteroids_Game_Runner-Game/Health.h Asteroids_Game_Runner-Game/states.h Asteroids_Game_Runner-Game/states.cpp Asteroids_Game_Runner-Game/Enums/statesEnum.h Asteroids_Game_Runner-Game/Health.cpp
-//        Asteroids_Game_Runner-Game/Health.h Asteroids_Game_Runner-Game/ScoreBoard2.h Asteroids_Game_Runner-Game/ScoreBoard2.cpp Asteroids_Game_Runner-Game/SpaceshipGun.cpp Asteroids_Game_Runner-Game/SpaceshipGun.h)
-//
-//
-//target_link_libraries(Final_Project sfml-graphics sfml-audio)
-//find_package(SFML 2.5 COMPONENTS graphics audio REQUIRED)
-
