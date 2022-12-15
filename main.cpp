aa66d39f4b35efbcf8002403a6feffa0de7c3080
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2022-12-14 17:39:03 -0800

    Remove Comments

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "Top_Hat_Guy_Runner.h"
 int main()
 {
+    //test
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
